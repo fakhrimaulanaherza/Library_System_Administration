f844cdee26aad3d46967fed8f0ee3edaad2f2854
fakhri_maulana_herza <fakhrimaulanaherza@gmail.com>
2025-02-06 11:16:46 +0700

    modified comment

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # ===================================
 # [Your Program Title]
 # ===================================
-# Developed by. [Enter your name]
+# Developed by. Fakhri Maulana Herza
 # JCDS - [Class Batch]
 
 
